efb4cf3d180a39f30ce53538cb4b9ad32cb9e3db
Paulo V C de Oliveira <paulovt7@gmail.com>
2019-04-18 21:05:19 +0100

    feat: inital powershell config

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,36 @@
+﻿# Ensure that Get-ChildItemColor is loaded
+Import-Module Get-ChildItemColor
+
+# Set l and ls alias to use the new Get-ChildItemColor cmdlets
+Set-Alias l Get-ChildItemColor -Option AllScope
+Set-Alias ls Get-ChildItemColorFormatWide -Option AllScope
+
+# Helper function to show Unicode characters
+function U
+{
+    param
+    (
+        [int] $Code
+    )
+ 
+    if ((0 -le $Code) -and ($Code -le 0xFFFF))
+    {
+        return [char] $Code
+    }
+ 
+    if ((0x10000 -le $Code) -and ($Code -le 0x10FFFF))
+    {
+        return [char]::ConvertFromUtf32($Code)
+    }
+ 
+    throw "Invalid character code $Code"
+}
+
+# Ensure posh-git is loaded
+Import-Module -Name posh-git
+
+# Ensure oh-my-posh is loaded
+Import-Module -Name oh-my-posh
+
+# Set the default prompt theme
+Set-Theme Paradox
